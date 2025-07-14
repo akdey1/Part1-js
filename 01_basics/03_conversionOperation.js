4d7d2936714ff355d484d72c84db85554ba578bd
Arnav Kumar Dey <deyarnav847@gmail.com>
2025-07-14 16:20:00 +0000

    updated

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33"
+
+// console.log(typeof score); 
+// console.log(typeof(score));  
+
+let valueInNumber = Number(score) 
+// console.log(typeof(valueInNumber)); 
+// console.log(valueInNumber);
+
+// "33" => 33 
+// "33abc" => NaN 
+// true => 1; false => 0 
+
+let isLoggedIn = 1 
+console.log(typeof isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn) 
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false 
+// "" => false 
+// "Arnav" => true 
+
+let number = 21051801
+
+console.log(typeof number);
+console.log(number);  
+
+let convertInString = String(number); 
+console.log(typeof convertInString); 
+console.log(convertInString);
