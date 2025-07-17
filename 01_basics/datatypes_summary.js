165e098ff4146ec0d22f4379df50146465b25192
Arnav Kumar Dey <deyarnav847@gmail.com>
2025-07-17 15:44:04 +0000

    datatype_summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,17 @@
+// primitive data types : 
+// 7 types : String, Number, boolean, null, undefined, Symbol, BigInt
+
+// Reference (Non primitive) types :
+// Arrays, Objects, functions  
+
+const heros = ["ironman","hulk","superman"]; 
+let myObj = {
+    name: "Arnav", 
+    age: 21,
+} 
+
+const myFunc = function(){
+   console.log("hi");
+} 
+console.log(myFunc);
+console.log(typeof heros);
