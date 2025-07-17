d48988f084a7f2a3b3c2190a6ce4c12fcc407125
Arnav Kumar Dey <deyarnav847@gmail.com>
2025-07-17 16:43:29 +0000

    stack and heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -15,3 +15,21 @@ const myFunc = function(){
 } 
 myFunc(); 
 console.log(typeof heros);
+
+let user1 = "hello i am Arnav" 
+let user2 = user1 
+user2 = "hello i am Arnav kumar dey" 
+
+console.log(user1); 
+console.log(user2);
+
+let myObjUser1 = {
+    email : "deyarnav847@gmail.com", 
+    age : 21, 
+};  
+
+let myObjUser2 = myObjUser1; 
+myObjUser2.email = "deyarnav874@gmail.com" 
+
+console.log(myObjUser1);
+console.log(myObjUser2);
