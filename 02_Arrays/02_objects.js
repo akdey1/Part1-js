1eea641466c50eb168e40eb1d566f6795c438740
Arnav Kumar Dey <deyarnav847@gmail.com>
2025-07-19 19:07:28 +0000

    update

---
+++ 02_Arrays/02_objects.js
@@ -0,0 +1,13 @@
+// objects
+let myUser = {
+    name : "Arnav", 
+    age : 18, 
+    email : "arnav@google.com",
+    isLoggedin : false, 
+    lastLogindays : ["monday","tuesday"],
+    "full-name" : "AKD"
+} 
+
+console.log(myUser);
+console.log(myUser["email"]);
+console.log(myUser["full-name"]);
