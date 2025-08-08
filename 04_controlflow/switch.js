0d61e8cf708155fc5f9c248171f1353653ead89b
Arnav Kumar Dey <deyarnav847@gmail.com>
2025-08-08 06:24:25 +0000

    switch

--- 04_controlflow/switch.js
+++ 04_controlflow/switch.js
@@ -1,2 +1,19 @@
 const month = 3 
 
+switch (month) {
+    case 1:
+        console.log("JANUARY");
+        break;
+    case 2:
+        console.log("FEBRUARY");
+        break;
+    case 3:
+        console.log("MARCH");
+        break;
+    case 4:
+        console.log("APRIL");
+        break;
+    default: 
+        console.log("DEFAULT");
+        break;
+}
