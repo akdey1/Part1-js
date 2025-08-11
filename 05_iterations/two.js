970275cec4fe227ebd75d894253d11632aa26819
Arnav Kumar Dey <deyarnav847@gmail.com>
2025-08-11 04:22:19 +0000

    while and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// while loop
+
+let index = 0
+while(index <= 10) {
+    console.log(`value of index is ${index}`)
+    index = index + 2; 
+} 
+
+let myArray = ['Akd','MKD','MD'] 
+
+let i = 0; 
+while(i <= myArray.length){
+    console.log(`value is ${myArray[i]}`); 
+    i++; 
+} 
+
+// do whhile 
+
+let score = 1
+do{
+    console.log(`score is ${score}`) 
+    score++; 
+}while(score <= 10); 
