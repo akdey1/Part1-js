8525a74672076f5811e6ad311cefb8ba4cc595e2
Arnav Kumar Dey <deyarnav847@gmail.com>
2025-12-10 16:38:08 +0000

    updated

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+alert() //Gives pop up when we use in the browser console  
+
+
+let name = "arnav" 
+let age = 22 
+let isLogged = false 
+let state; 
+
+// number => 2 to power 53 
+// bigint 
+// string 
+// boolean => true/false 
+// null => standalone value 
+// undefined =>   
+// symbol => unique 
+// object 
+
+console.log(typeof undefined) //o.p undefined 
+console.log(typeof null) //o.p object
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = 33 
+
+console.log(typeof score); 
+console.log(typeof(score));
+
+let valueInNumber = Number(score); 
+console.log(typeof valueInNumber) 
+console.log(valueInNumber); 
+
+//"33" => 33 
+//"33abc" => NaN 
+//true => 1; false => 0 
+//"" => false 
+//"shgfyugh" (if its filled or i say that its not empty string) => true
+
+let isLoggedIn = 1 
+let booleanIsLoggedIn = Boolean(isLoggedIn) 
+
+// coverting a number into string 
+let someNumber = 33 
+
+let StringNumber = String(someNumber); 
+console.log(someNumber); 
+console.log(typeof StringNumber); 
