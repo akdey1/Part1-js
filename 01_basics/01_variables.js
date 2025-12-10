c5246a0413712e8fa712eba9a6a8234438b95e05
Arnav Kumar Dey <deyarnav847@gmail.com>
2025-12-10 11:45:41 +0000

    updated

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553 
+let accountEmail = "arnav@google.com" 
+var accountPassword = "12345" 
+accountCity = "jaipur" 
+
+//accountId = 2 
+//const keyword value cannot be changed after initialized 
+
+console.log(accountId); 
+accountEmail = "hc@hc.com" 
+accountPassword = "21212121" 
+accountCity = "bengaluru"
+
+console.log(accountId); 
+
+console.table([accountId, accountEmail,accountPassword, accountCity])
