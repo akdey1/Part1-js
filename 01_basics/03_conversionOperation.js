101c5062796d750a050a236cc39d93d306e0de69
Arnav Kumar Dey <deyarnav847@gmail.com>
2025-12-15 09:57:00 +0000

    dataType_Updated

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,30 @@ let someNumber = 33
 let StringNumber = String(someNumber); 
 console.log(someNumber); 
 console.log(typeof StringNumber); 
+
+// operations 
+
+let value = 3 
+let negValue = -value 
+
+//console.log(2+2) 
+//console.log(2-2) 
+//console.log(2*2) 
+//console.log(2**2)  
+
+let str1 = "hello" 
+let str2 = "hitesh" 
+
+let str3 = str1 + str2 
+console.log(str3); 
+
+console.log("1" + 2); 
+console.log(1 + "2"); 
+console.log("1"+2+2); 
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2 
+
+let gameCouter = 100
+gameCounter++ 
+console.log(gameCounter); 
